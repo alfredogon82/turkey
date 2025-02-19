92c4997f7fd286e44c2ca638d02cd4794818ea56
Alfredo Gonzalez <alfredogon82@yahoo.com>
2025-02-18 21:08:29 -0500

    initial boilerplate - turkey challenge

---
+++ views/list.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alfredo González Turkey Challenge</title>
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1 class="display-4">Alfredo González - Turkey Challenge</h1>
+        <table class="table table-hover table-bordered">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="table-primary">
+                    <th scope="row">1</th>
+                    <td>Alfredo González</td>
+                    <td>alfredogon82@gmail.com</td>
+                </tr>
+                <tr class="table-primary">
+                    <th scope="row">2</th>
+                    <td>This is</td>
+                    <td>a Bootstrap</td>
+                </tr>
+                <tr class="table-primary">
+                    <th scope="row">3</th>
+                    <td>Boilerplate</td>
+                    <td>Example</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <!-- jQuery and Bootstrap JS -->
+    <script src="assets/js/jquery-3.5.1.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
